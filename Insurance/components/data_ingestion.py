185d50a656b3ccb4b1d871d7b0d316a55434e883
Vikash Samal <vks.samal1992@gmail.com>
2023-04-08 01:34:22 +0530

    Data ingestion

---
+++ Insurance/components/data_ingestion.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import os, sys
+from Insurance.exception import InsuranceException
+from Insurance.entity import artifact_entity
+from Insurance.entity import config_entity
+from Insurance import utils
+from Insurance.logger import logging
+from sklearn.model_selection import train_test_split
+
+
+
+class DataIngestion: # data will be divided to Train, Test and Validate.
+    def __init__(self, data_ingestion_config: config_entity.DataIngestionConfig) -> None:
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise InsuranceException(e, sys)
+        
+    def initiate_data_ingestion(self)-> artifact_entity.DataIngestionArtifact:
+        try:
+            logging.info("Export collection data as pandas dataframe")
+            df:pd.DataFrame= utils.get_collection_as_dataframe(
+                database_name= self.data_ingestion_config.database_name,
+                collection_name=self.data_ingestion_config.collection_name)
+            logging.info("Save data in future store")
+
+            # Replace NA value with NAN.
+            df.replace(to_replace="na", value= np.NAN, inplace=True)
+
+            # Save data in future store
+            logging.info("Create feature store folder if not available.")
+            feature_store_dir = os.path.dirname(self.data_ingestion_config.feature_store_file_path)
+            os.makedirs(feature_store_dir, exist_ok=True)
+
+            logging.info("Save df to feature store folder") 
+            # Save df to feature store folder   
+            df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path, index=False, header=  True)
+            
+            logging.info("Spliting our data into train and test set.")
+            train_df, test_df = train_test_split(df, test_size=self.data_ingestion_config.test_size, random_state=1)
+
+            logging.info("Create dataset directory folder if not exists")    
+            dataset_df = os.path.dirname(self.data_ingestion_config.train_file_path)
+            os.makedirs(dataset_df, exist_ok=True)
+
+            logging.info("Save dataset to feature store folder.")
+            train_df.to_csv(path_or_buf = self.data_ingestion_config.train_file_path, index = False,  header = True)
+            test_df.to_csv(path_or_buf = self.data_ingestion_config.test_file_path, index = False,  header = True)
+
+            # Prepare artifact folder
+            data_ingestion_artifact = artifact_entity.DataIngestionArtifact (
+                feature_store_file_path = self.data_ingestion_config.feature_store_file_path,
+                train_file_path = self.data_ingestion_config.train_file_path,
+                trst_file_path = self.data_ingestion_config.test_file_path
+                )
+
+
+        except Exception as e:
+            raise InsuranceException(e, sys)
+        
+
+
+
+
+
+
+
